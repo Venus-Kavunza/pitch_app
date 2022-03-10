82137c7f3594d854b4b526f05d2883e7564ff3ee
Venus-Kavunza <venusmwende@gmail.com>
2022-03-10 12:21:09 +0300

    it will deploy

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,8 +4,6 @@ from ..models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
 from flask_login import login_user,logout_user,login_required
-from .forms import RegistrationForm
-from .. import db
 from ..email import mail_message
 
 
