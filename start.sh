e690d074835502d3aa19d4fcc4875505dc2a9bd4
Venus-Kavunza <venusmwende@gmail.com>
2022-03-11 11:38:29 +0300

    It will deploy again

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export DATABASE_URL='postgresql+psycopg2://moringa:Venusstudy03.@localhost/pitch'
-export MAIL_USERNAME='venusmwende@gmail.com'
-export MAIL_PASSWORD='Venus1204'
+export MAIL_USERNAME='fayimani693@gmail.com'
+export MAIL_PASSWORD='Venusstudy03.'
 export SECRET_KEY='muya'
 python3.8 manage.py server
